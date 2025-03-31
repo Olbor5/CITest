deeb80e7533da37386a2c7d2383a99bcad6d837d
olbor5 <borgstromm@hotmail.com>
2025-03-31 15:23:00 +0200

    Initial commit

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+import user.User;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserTest {
+
+    @Test
+    public void testCreateUser() {
+        User user = new User("Staffan", "password123");
+    }
+
+    @Test
+    public void testGetUserName() {
+        User user = new User("Staffan", "password123");
+        String expected = "Staffan";
+        String actual = user.getUserName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetPassword() {
+        User user = new User("Staffan", "password123");
+        String expected = "password123";
+        String actual = user.getPassword();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetUserName() {
+        User user = new User("Staffan", "password123");
+        String expected = "Henry";
+        user.setUserName("Henry");
+        String actual = user.getUserName();
+        assertEquals(expected, actual);
+    }
+
+}
